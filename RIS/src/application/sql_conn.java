2471f42e8328454f91fdde5d77f8efe160282ca0
jfellis314 <69335598+jfellis314@users.noreply.github.com>
2022-04-08 04:08:40 -0400

    Create sql_conn.java
    Old connectivity file

---
+++ RIS/src/application/sql_conn.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class sql_conn {
+
+    public static void main(String[] args)
+    {
+        System.out.println(establishConn());
+    }
+	public static Object establishConn() {
+		String url = "jdbc:mysql://localhost:3306/db_ris";
+		String username = "root";
+		String password = "toor";
+
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			return connection;
+		} catch (Exception e) {
+
+		}
+		return new String("Error connecting!");
+	}
+}
